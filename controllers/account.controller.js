e87623abad64e25e700b5a154585781f0129b556
LeCheWang <haphonggoroki@gmail.com>
2023-08-25 19:51:33 +0700

    phu2

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -6,5 +6,21 @@ module.exports = {
         console.log(body);
         const newAccount = await accountModel.create(body);
         return res.status(200).json(newAccount);
+    },
+    getAccounts: async (req, res) => {
+        const accounts = await accountModel.find();
+        return res.status(200).json(accounts);
+    },
+    updateAccount: async (req, res) => {
+        const id = req.params.id;
+        const body = req.body;
+
+        const updatedAccount = await accountModel.findByIdAndUpdate(id, body, {new: true});
+        return res.status(200).json(updatedAccount);
+    },
+    deleteAccount: async (req, res) => {
+        const id = req.params.id;
+        const deletedAccount = await accountModel.findByIdAndDelete(id);
+        return res.status(200).json(deletedAccount);
     }
 } 
--- routers/account.router.js
+++ routers/account.router.js
@@ -2,11 +2,20 @@ const express = require("express")
 const router = express.Router()
 
 const {
-    createAccount
+    createAccount,
+    getAccounts,
+    updateAccount,
+    deleteAccount
 } = require("../controllers/account.controller")
 
 router
     .route("/")
+    .get(getAccounts)
     .post(createAccount);
+
+router
+    .route("/:id")
+    .patch(updateAccount)
+    .delete(deleteAccount);
 
 module.exports = router;
